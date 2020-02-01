a45e7b3399b2624439f9bee84f86e9e745bda0f0
hbsnow <yuki.4uing@gmail.com>
2020-02-01 10:29:54 +0900

    fix: rename

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,44 +1,44 @@
 import Link from 'next/link'
-import { SerializedStyles } from '@emotion/core'
+import { SerializedStyles, css } from '@emotion/core'
 
-//const cachedStyles = css``
+const buttonCss = css``
 
 const isLinkType = (props): props is LinkProps => props?.href !== undefined
 
 const Button: React.FC<ButtonProps | LinkProps> = (props) => {
   if (isLinkType(props)) {
-    const { children, style, href, as, amp, ...restProps } = props
+    const { children, css, href, as, amp, ...restProps } = props
     const isExternal = href.startsWith('http')
     return amp || isExternal ? (
-      <a href={href} css={style} {...restProps}>
+      <a href={href} css={[buttonCss, css]} {...restProps}>
         {children}
       </a>
     ) : (
       <Link href={href} {...(as && { as })}>
-        <a css={style} {...restProps}>
+        <a css={[buttonCss, css]} {...restProps}>
           {children}
         </a>
       </Link>
     )
   }
 
-  const { children, style, ...restProps } = props
+  const { children, css, ...restProps } = props
   return (
-    <button type="button" css={style} {...restProps}>
+    <button type="button" css={css} {...restProps}>
       {children}
     </button>
   )
 }
 
 type LinkProps = {
-  style?: SerializedStyles
+  css?: SerializedStyles
   href: string
   as?: string
   amp?: boolean
 } & JSX.IntrinsicElements['a']
 
 type ButtonProps = {
-  style?: SerializedStyles
+  css?: SerializedStyles
 } & JSX.IntrinsicElements['button']
 
 export default Button

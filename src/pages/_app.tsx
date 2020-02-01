4c54f0ef1acbde919191181d30ea110c5df46846
hbsnow <yuki.4uing@gmail.com>
2020-02-02 00:17:32 +0900

    feat: whyDidYouRenderの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
+import whyDidYouRender from '@welldone-software/why-did-you-render'
+
+if (process.env.NODE_ENV !== 'production') {
+  whyDidYouRender(React)
+}
 
 export const config = { amp: true }
 
---
+++ src/pages/blog.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import DefaultLayout from '../layouts/DefaultLayout/DefaultLayout'
+
+const Blog = (): JSX.Element => {
+  return <DefaultLayout>blog</DefaultLayout>
+}
+
+export default Blog

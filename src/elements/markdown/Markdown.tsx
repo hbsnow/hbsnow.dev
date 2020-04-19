870faacc141730f4ebd1c5c0260482e7e1280c56
hbsnow <yuki.4uing@gmail.com>
2020-04-19 22:22:40 +0900

    feat: ToCの出力

--- src/elements/markdown/Markdown.tsx
+++ src/elements/markdown/Markdown.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { Children } from 'react'
 
 import { imageSize } from 'image-size'
+import { onlyText } from 'react-children-utilities'
 import ReactMarkdown from 'react-markdown'
 import remarkSectionize from 'remark-sectionize'
+import remarkToc from 'remark-toc'
 
 import Blockcode from '../blockcode/Blockcode'
 import Heading from '../heading/Heading'
@@ -10,6 +12,11 @@ import Heading from '../heading/Heading'
 type Props = {
   source: string
 } & JSX.IntrinsicElements['div']
+
+const isToc = (children?: React.ReactNode): boolean => {
+  if (!children) return false
+  return onlyText(children) === '目次'
+}
 
 const Markdown: React.FC<Props> = ({ source, ...restProps }) => {
   const code: React.FC<{ language: string; value: string }> = ({
@@ -20,11 +27,19 @@ const Markdown: React.FC<Props> = ({ source, ...restProps }) => {
   }
 
   const section: React.FC = ({ children }) => {
+    if (isToc(Children.toArray(children)?.[0])) {
+      return <section className="toc">{children}</section>
+    }
     return <section>{children}</section>
   }
 
   const heading: React.FC<{ level: string }> = ({ level, children }) => {
-    return <Heading level={parseInt(level)}>{children}</Heading>
+    const headingLevel = parseInt(level)
+    if (headingLevel === 2 && isToc(children)) {
+      return null
+    }
+
+    return <Heading level={headingLevel}>{children}</Heading>
   }
 
   const image: React.FC<{ src: string; alt: string }> = ({ src, alt }) => {
@@ -51,12 +66,21 @@ const Markdown: React.FC<Props> = ({ source, ...restProps }) => {
   return (
     <div data-testid="Markdown" className="markdown" {...restProps}>
       <ReactMarkdown
-        source={source}
-        plugins={[remarkSectionize]}
+        source={`## 目次\n${source}`}
+        plugins={[
+          [
+            remarkToc,
+            {
+              heading: '目次',
+              maxDepth: 3,
+            },
+          ],
+          remarkSectionize,
+        ]}
         renderers={{
           code,
-          section,
           heading,
+          section,
           image,
         }}
       />

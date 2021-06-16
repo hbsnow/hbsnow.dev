37d9fc7e02399601e57977326a9244bbe6b256e3
hbsnow <yuki.4uing@gmail.com>
2021-06-17 02:08:51 +0900

    fix: webpack5

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ const config = {
     "!**/*.d.ts",
     "!**/node_modules/**",
   ],
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",

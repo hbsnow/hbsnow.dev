97c9f2e68c8ecba0a7611147549abb3f97b9d5b6
hbsnow <yuki.4uing@gmail.com>
2022-10-29 10:18:45 +0900

    feat: add sns icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,29 @@
 import type { ComponentPropsWithoutRef } from "react";
 
-import clsx from "clsx";
-
 import styles from "./styles.module.css";
+import { Container } from "@/cores/Container";
+import { SystemIcon } from "@/cores/Icon";
+import { Link } from "@/cores/Link";
 
 type Props = ComponentPropsWithoutRef<"footer">;
 
 export const Footer = (props: Props): JSX.Element => {
-  const { className, ...rest } = props;
-
   return (
-    <footer className={clsx(className, styles.root)} {...rest}>
-      © 2022 hbsnow
+    <footer {...props}>
+      <Container>
+        <div className={styles.container}>
+          <div className={styles.sns}>
+            <Link href="https://github.com/hbsnow" external>
+              <SystemIcon name="github" aria-label="GitHub" />
+            </Link>
+            <Link href="https://twitter.com/hbsnow" external>
+              <SystemIcon name="twitter" aria-label="Twitter" />
+            </Link>
+          </div>
+
+          <div className={styles.copyright}>© 2022 hbsnow</div>
+        </div>
+      </Container>
     </footer>
   );
 };

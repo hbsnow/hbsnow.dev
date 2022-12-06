a6bdf8711c3119fe44243a81fa2d940d4093c6db
hbsnow <yuki.4uing@gmail.com>
2022-12-06 14:29:31 +0900

    seat: sectionize

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,14 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import rehypeWrapAll from "rehype-wrap-all";
 import remarkLinkCard from "remark-link-card";
+import remarkSectionize from "remark-sectionize";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hbsnow.dev",
   integrations: [react(), sitemap()],
   markdown: {
-    remarkPlugins: [remarkLinkCard],
+    remarkPlugins: [remarkLinkCard, remarkSectionize],
     rehypePlugins: [
       [
         rehypeWrapAll,
--- src/types/markdown.d.ts
+++ src/types/markdown.d.ts
@@ -1,2 +1,3 @@
 declare module "remark-link-card";
+declare module "remark-sectionize";
 declare module "rehype-wrap-all";
